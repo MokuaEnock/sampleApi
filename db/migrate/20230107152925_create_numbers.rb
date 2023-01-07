ce36c0696b94fb93db7b5015bdf6d53142c4fcb2
Enock <enock.mokua@student.moringaschool.com>
2023-01-07 18:32:42 +0300

    add attributes to user

--- db/migrate/20230107152925_create_numbers.rb
+++ db/migrate/20230107152925_create_numbers.rb
@@ -2,9 +2,9 @@ class CreateNumbers < ActiveRecord::Migration[7.0]
   def change
     create_table :numbers do |t|
       t.belongs_to :user, foreign_key: true
-      t.string :name
-      t.string :email
-      
+      t.string :first
+      t.string :second
+
       t.timestamps
     end
   end
