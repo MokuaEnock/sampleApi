e05d5ca66bb09d464e391ff946223b421a57239a
Enock <enock.mokua@student.moringaschool.com>
2023-01-07 18:53:13 +0300

    create actions in controllers

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,2 +1,16 @@
 class NumbersController < ApplicationController
+  def index
+    render json: Number.all
+  end
+
+  def create
+    num = Number.create(num_params)
+    render json: num
+  end
+
+  private
+
+  def num_params
+    params.permit(:first, :second)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def index
+    render json: User.all
+  end
+
+  def create
+    user = User.create(user_params)
+    render json: user
+  end
+
+  def show
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
+  end
 end
